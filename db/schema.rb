998ae0246b64a247d62e6418e5b5f408f58e367a
Ivan Arakcheev <wildfiler0@gmail.com>
2016-06-01 13:59:12 +0300

    Add quiet_assets and activerecord_sane_schema_dumper gems
    - Add quiet_assets gem to make development logs clearer
    - Add activerecord_sane_schema_dumper to remove problems with unrelated changes in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -17,39 +17,39 @@ ActiveRecord::Schema.define(version: 20160528224738) do
   enable_extension "plpgsql"
 
   create_table "components", force: :cascade do |t|
-    t.integer  "receipt_id",    null: false
-    t.integer  "ingredient_id", null: false
-    t.integer  "quantity"
-    t.string   "units"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer "receipt_id", null: false
+    t.integer "ingredient_id", null: false
+    t.integer "quantity"
+    t.string "units"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ingredients", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "parent_id"
-    t.string   "name",       null: false
-    t.boolean  "spice",      null: false
-    t.boolean  "vegeterian", null: false
+    t.integer "user_id"
+    t.integer "parent_id"
+    t.string "name", null: false
+    t.boolean "spice", null: false
+    t.boolean "vegeterian", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "receipts", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "title",       null: false
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "user_id"
+    t.string "title", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",      null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "encrypted_password", limit: 128
-    t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
